46dc72d99c200ec25b408b26fc74050f73b0ae9f
Renan Nascimento <atomsfrontend@gmail.com>
2025-07-23 21:37:30 -0300

    feat: get data to render component single post from data of post

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,7 +1,18 @@
 "use server";
 
+// React
+import { Suspense } from "react";
+
+// Components
+import { SinglePost } from "@/app/components/SinglePost";
+import { SpinLoader } from "@/app/components/SpinLoader";
+
+// Query Cache
+import { findPostBySlugCache } from "@/lib/post/queries";
+
 // Next
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
+import { Metadata } from "next";
 
 type PostSlugPageProps = {
   params: Promise<{
@@ -9,22 +20,52 @@ type PostSlugPageProps = {
   }>;
 };
 
-export default async function PostSlugPage({ params }: PostSlugPageProps) {
-  // Note: params is a Promise, so we need to await it
-  // If you are using a framework that provides params directly,
-  // you might not need to await it.
-  // For example, in Next.js, params is already resolved.
-  // Here, we assume params is a Promise that resolves to an object with a slug property
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
   const { slug } = await params;
+  const post = await findPostBySlugCache(slug);
 
+  if (!post || !slug) {
+    throw new Error("Post or Slug can be not exists.");
+  }
+
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
+}
+
+// Note: params is a Promise, so we need to await it
+// If you are using a framework that provides params directly,
+// you might not need to await it.
+// For example, in Next.js, params is already resolved.
+// Here, we assume params is a Promise that resolves to an object with a slug property
+export default async function PostSlugPage({ params }: PostSlugPageProps) {
+  const { slug } = await params;
   if (!slug) {
     redirect("/");
   }
 
+  const post = await findPostBySlugCache(slug);
+  if (!post) {
+    notFound();
+  }
+
   return (
-    <div className="h-full bg-slate-500">
-      <h1>post/{slug}</h1>
-      <p>This is the post page content for {slug}.</p>
-    </div>
+    <Suspense
+      fallback={<SpinLoader SpinLoaderContainerClass="h-[calc(100vh-64px)]" />}
+    >
+      <SinglePost
+        slug={slug}
+        title={post.title}
+        author={post.author}
+        createdAt={post.createdAt}
+        altCoverImageUrl={post.title}
+        coverImageUrl={post.coverImageUrl}
+        excerpt={post.excerpt}
+        content={post.content}
+      />
+    </Suspense>
   );
 }
