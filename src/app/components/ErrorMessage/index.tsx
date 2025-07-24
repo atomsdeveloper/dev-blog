23ab9bb83dbda65300dc4fc530032b706548ddd1
Renan Nascimento <atomsfrontend@gmail.com>
2025-07-23 21:35:21 -0300

    feat: created component error message

---
+++ src/app/components/ErrorMessage/index.tsx
@@ -0,0 +1,13 @@
+type ErrorMessageProps = {
+  title: string;
+  text: string;
+};
+
+export function ErrorMessage({ title, text }: ErrorMessageProps) {
+  return (
+    <div className="min-h-[320px] flex flex-col items-center justify-center bg-slate-100 mb-24 p-8 rounded-4xl">
+      <h1 className="text-6xl/normal font-extrabold text-slate-900">{title}</h1>
+      <p className="text-lg text-slate-400">{text}</p>
+    </div>
+  );
+}
