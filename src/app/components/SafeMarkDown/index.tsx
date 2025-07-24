575f594288a014edb7b8763c6307070bafc2ba25
Renan Nascimento <atomsfrontend@gmail.com>
2025-07-23 21:34:15 -0300

    feat: created component markdown

---
+++ src/app/components/SafeMarkDown/index.tsx
@@ -0,0 +1,32 @@
+// React Markdown
+import ReactMarkdown from "react-markdown";
+import rehypeSanitize from "rehype-sanitize";
+import remarkGfm from "remark-gfm";
+
+type SafeMarkdownProps = {
+  markdown: string;
+};
+
+export function SafeMarkdonw({ markdown }: SafeMarkdownProps) {
+  return (
+    <div className="prose prose-slate w-full max-w-none overflow-hidden prose-a:transition prose-a:no-underline prose-a:hover:underline prose-a:text-blue-500 prose-a:hover:text-blue-700 prose-img:mx-auto md:prose-lg">
+      <ReactMarkdown
+        rehypePlugins={[rehypeSanitize]}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ node, ...props }) => {
+            if (!node?.children) return "";
+
+            return (
+              <div className="overflow-x-auto">
+                <table className="w-full min-w-[600px]" {...props} />
+              </div>
+            );
+          },
+        }}
+      >
+        {markdown}
+      </ReactMarkdown>
+    </div>
+  );
+}
