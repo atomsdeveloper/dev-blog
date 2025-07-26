c6b1d940a97547d4901a8fc12ea546b440440597
Renan Nascimento <atomsfrontend@gmail.com>
2025-07-25 21:48:05 -0300

    feat: rename and add new methods

--- src/repositories/post/post-repository.ts
+++ src/repositories/post/post-repository.ts
@@ -3,7 +3,10 @@ import { PostModel } from "@/model/post/post-model";
 
 // Repository Pattern
 export interface PostRepository {
-  findAllPublishTrue(): Promise<PostModel[]>;
+  findAllPublishedTrue(): Promise<PostModel[]>;
+  findAll(): Promise<PostModel[]>;
+  findBySlugPublishedTrue(slug: string): Promise<PostModel>;
   findBySlug(slug: string): Promise<PostModel>;
+  findByIdPublishedTrue(id: string): Promise<PostModel>;
   findById(id: string): Promise<PostModel>;
 }
