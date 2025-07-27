d1960af04c8a25fc89490792217ef694764df53d
Renan Nascimento <atomsfrontend@gmail.com>
2025-07-27 18:54:32 -0300

    feat: created post page to admin route

--- src/app/admin/post/page.tsx
+++ src/app/admin/post/page.tsx
@@ -1,5 +1,21 @@
+import { findAllPostCache } from "@/lib/post/queries/admin";
+import { Metadata } from "next";
+
 export const dynamic = "force-dynamic";
 
+export const metadata: Metadata = {
+  title: "Admin",
+};
+
 export default async function PostPage() {
-  return <div className="py-16 text-6xl">Post Page</div>;
+  const posts = await findAllPostCache();
+
+  return (
+    <div className="py-16 text-xl">
+      <h1>Posts Page</h1>
+      {posts.map((post) => {
+        return <div key={post.id}>{post.title}</div>;
+      })}
+    </div>
+  );
 }
