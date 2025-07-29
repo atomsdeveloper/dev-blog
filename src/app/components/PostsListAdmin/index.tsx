f575331417562fa615b56c9787f0abfab35a451a
Renan Nascimento <atomsfrontend@gmail.com>
2025-07-29 20:10:03 -0300

    feat: created component to render posts in admin page

--- src/app/components/PostsListAdmin/index.tsx
+++ src/app/components/PostsListAdmin/index.tsx
@@ -2,42 +2,48 @@
 import { findAllPublishTrueCache } from "@/lib/post/queries/published";
 
 // Components
-import { PostCoverImage } from "../PostCoverImage";
-import { PostSummary } from "../PostSummary";
 import { PostModel } from "@/model/post/post-model";
 
+// Next
+import Link from "next/link";
+
+// Icons
+import { Trash2Icon } from "lucide-react";
+
 export async function PostsListAdmin() {
-  const posts: PostModel[] = ((await findAllPublishTrueCache()) ?? []).slice(1);
+  const posts: PostModel[] = (await findAllPublishTrueCache()) ?? [];
 
   if (!posts || posts.length === 0) {
     return <div className="p-4">No posts available.</div>;
   }
 
   return (
-    <section className="grid grid-cols-1 mb-16 sm:grid-cols-2 md:grid-cols-3 gap-8">
+    <section className="mb-16">
       {posts.map((post) => {
-        const { id, coverImageUrl, title, slug, createdAt, excerpt } = post;
+        const { id, published } = post;
+
+        const wasPublished = !published && "bg-slate-300";
 
         return (
-          <div key={id} className="flex flex-col gap-4 group">
-            <PostCoverImage
-              image={{
-                width: 1200,
-                height: 720,
-                src: `${coverImageUrl}`,
-                alt: `Cover Image ${title}`,
-              }}
-              link={{ href: `/post/${slug}` }}
-            />
+          <section
+            key={id}
+            className={`${wasPublished} flex gap-2 items-center justify-between`}
+          >
+            <Link href={`/admin/post/${id}`} />
+            {!wasPublished && (
+              <span className="text-xs text-slate-600 italic">
+                (Post não publicado.)
+              </span>
+            )}
 
-            <PostSummary
-              as="h2"
-              link={{ href: `/post/${slug}` }}
-              createdAt={createdAt}
-              title={title}
-              excerpt={excerpt}
-            />
-          </div>
+            <button
+              title="Apagar post"
+              aria-label="Apagar post."
+              className="text-red-500 cursor-pointer transition hover:scale-125 hover:text-red-600 [&_svg]:w-4 [&_svg]:h-4"
+            >
+              <Trash2Icon />
+            </button>
+          </section>
         );
       })}
     </section>
