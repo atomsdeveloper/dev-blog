591ad251851f4af583de1d092563faf9855a4806
Renan Nascimento <atomsfrontend@gmail.com>
2025-07-31 21:26:39 -0300

    feta: created action to delete post

--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -1,5 +1,44 @@
 "use server";
 
+// Database
+import { drizzleDatabase } from "@/db/drizzle";
+
+// Queries Cache Admin
+import { findPostByIdCache } from "@/lib/post/queries/admin";
+
+// SQL Queries
+import { eq } from "drizzle-orm";
+
+// Schemas
+import { postsTable } from "@/db/drizzle/schemas";
+
+// Repositories
+import { revalidateTag } from "next/cache";
+
 export async function deletePostAction(id: string) {
-  return id;
+  if (!id || typeof id !== "string") {
+    return {
+      error: true,
+      message: "Invalid post ID.",
+    };
+  }
+
+  const post = await findPostByIdCache(id).catch(() => undefined);
+  if (!post) {
+    return {
+      error: true,
+      message: "Post not found.",
+    };
+  }
+
+  // Mover para o repositório
+  await drizzleDatabase.delete(postsTable).where(eq(postsTable.id, id));
+
+  revalidateTag("posts");
+  revalidateTag(`post-${post.slug}`);
+
+  return {
+    error: false,
+    message: "Post deleted successfully.",
+  };
 }
