969b030ab3583a95f95f9fa9e2944b363bfc7c3a
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-01 21:35:14 -0300

    feta: created component Button to app

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/app/components/Button";
+
 export const dynamic = "force-dynamic";
 
 export default async function NewPostPage() {
-  return <div className="py-16 text-6xl">New Post Page</div>;
+  return (
+    <div className="py-16 text-6xl">
+      <Button type="submit">Button</Button>
+    </div>
+  );
 }
