4f9972c56724dda5fed4335db9cb1077a5f49c67
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-03 14:08:08 -0300

    feat: created component InputText

---
+++ src/app/components/InputText/index.tsx
@@ -0,0 +1,25 @@
+// Hooks
+import { useId } from "react";
+
+type InputTextProps = {
+  labelText: string;
+} & React.ComponentProps<"input">;
+
+export function InputText({ labelText, ...props }: InputTextProps) {
+  const id = useId(); // Generate a unique ID for the input element in each render this component is used
+
+  return (
+    <div className="flex flex-col gap-2 m-1">
+      {labelText != "" && (
+        <label className="text-sm" htmlFor={id}>
+          {labelText}
+        </label>
+      )}
+      <input
+        className={`bg-white text-base/tight outline-none ring-1 ring-gray-300 p-2 rounded transition focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed disabled:placeholder:text-gray-400 disabled:bg-slate-100 placeholder:text-slate-500 ${props.className}`}
+        {...props}
+        name={id}
+      />
+    </div>
+  );
+}
