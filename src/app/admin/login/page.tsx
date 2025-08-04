db5575f6d7febce6531a9515b7a4cd7f5a9aee58
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-03 22:02:03 -0300

    chore: add comments

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic";
 export default async function Login() {
   return (
     <div className="py-16 text-6xl">
-      <ToastifyContainer /> {/* Para possível popup de toast */}
+      <ToastifyContainer /> {/* Para possível popup de toast se necessário*/}
     </div>
   );
 }
