bab1181b3097bbb2bc3e1e16dafc84c2ffbc736e
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-16 20:21:21 -0300

    feat: add button on click for add image

--- src/app/admin/components/ImageUploader/index.tsx
+++ src/app/admin/components/ImageUploader/index.tsx
@@ -1,9 +1,40 @@
 "use client";
 
+// Components
+import { Button } from "@/app/components/Button";
+import { ImageUpIcon } from "lucide-react";
+
+// Hooks
+import { useRef } from "react";
+
 export const ImageUploader = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClickButton = () => {
+    if (inputRef.current === null) return;
+
+    inputRef.current.click();
+  };
   return (
-    <div className="flex flex-col gap-2">
-      <input name="file" type="file" />
+    <div className="flex flex-col gap-2 text-sm">
+      <Button
+        onClick={handleClickButton}
+        size="md"
+        type="button"
+        variant="default"
+        className="flex items-center"
+      >
+        <ImageUpIcon />
+        Selecionar uma imagem
+      </Button>
+
+      <input
+        ref={inputRef}
+        name="file"
+        type="file"
+        accept="image/*"
+        className="hidden"
+      />
     </div>
   );
 };
