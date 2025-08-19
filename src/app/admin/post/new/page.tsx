dc1adc2b5b63d3116cf441254d59bf6f7276e62e
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-18 21:51:05 -0300

    fix: html and css

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -4,5 +4,10 @@ import { Form } from "../../../admin/components/Form";
 export const dynamic = "force-dynamic";
 
 export default async function NewPostPage() {
-  return <Form />;
+  return (
+    <div className="py-16 text-6xl">
+      <h1 className="text-xs">Criar Post</h1>
+      <Form />
+    </div>
+  );
 }
