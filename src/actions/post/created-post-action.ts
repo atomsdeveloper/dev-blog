23f89ec1611c228fb4361c2d10d0bc395b318a68
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-19 22:04:42 -0300

    feat: created action from insert new post

--- src/actions/post/created-post-action.ts
+++ src/actions/post/created-post-action.ts
@@ -1,13 +1,77 @@
+"use server";
+
+// Data Transfer Object
+import { drizzleDatabase } from "@/db/drizzle";
+import { postsTable } from "@/db/drizzle/schemas";
+import { dtoPostNotNull, PostDataTransferObjectType } from "@/dto/post/dto";
+
+// Check
+import { PostCreateSchema } from "@/lib/post/check";
+
+// Model
+import { PostModel } from "@/model/post/post-model";
+
+// Utils
+import { getZodConvertErrorMessageArray } from "@/utils/get-zod-error-msg";
+import { makeSlugTextString } from "@/utils/make-slug-text-string";
+import { revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
+
+//
+import { v4 as uuid } from "uuid";
+
 type CreatePostActionProps = {
-  number: number;
+  valuesFormState: PostDataTransferObjectType; // Valores recebidos
+  errors: string[];
+  success?: string;
 };
 
 export async function createdPostAction(
-  prevState: CreatePostActionProps
+  prevState: CreatePostActionProps,
+  formData: FormData // Valores enviados para serem salvos e manipulados.
 ): Promise<CreatePostActionProps> {
-  console.log({ prevState });
+  console.log("Create Post");
+  // TODO: Check has user logged.
 
-  return {
-    number: (prevState.number += 1),
+  if (!(formData instanceof FormData)) {
+    return {
+      valuesFormState: { ...prevState.valuesFormState },
+      errors: ["Os dados recebidos são inválidos."],
+    };
+  }
+
+  const convertFormDataEntriesTypeForObj = Object.fromEntries(
+    formData.entries()
+  );
+
+  const returnZodCheckDatasForm = PostCreateSchema.safeParse(
+    convertFormDataEntriesTypeForObj
+  );
+
+  if (!returnZodCheckDatasForm.success) {
+    const arrayErrorsZod = getZodConvertErrorMessageArray(
+      returnZodCheckDatasForm.error.format()
+    ).filter(Boolean);
+
+    return {
+      errors: arrayErrorsZod,
+      valuesFormState: dtoPostNotNull(convertFormDataEntriesTypeForObj),
+    };
+  }
+
+  const checkAllDatasOk = returnZodCheckDatasForm.data;
+
+  const newPost: PostModel = {
+    ...checkAllDatasOk,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    id: uuid(),
+    slug: makeSlugTextString(checkAllDatasOk.title),
   };
+
+  // TODO: Move action that manipulate the removing of posts. /repositories/post/drizzle-post-repository.ts
+  await drizzleDatabase.insert(postsTable).values(newPost);
+
+  revalidateTag("posts");
+  redirect(`/post/${newPost.id}`);
 }
