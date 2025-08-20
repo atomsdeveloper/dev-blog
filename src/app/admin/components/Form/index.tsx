06f3bd6b6d97cff6d81592c4ad2be750914bc669
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-19 22:03:35 -0300

    fix: form page from receive datas and show errors or success on send datas

--- src/app/admin/components/Form/index.tsx
+++ src/app/admin/components/Form/index.tsx
@@ -8,57 +8,110 @@ import { MarkdownEditor } from "../../../components/MarkdownEditor";
 import { ImageUploader } from "../ImageUploader";
 
 // React
-import { useActionState, useState } from "react";
+import { useActionState, useEffect, useState } from "react";
 
 // Type
-import { PostDataTransferObjectType } from "@/dto/post/dto";
+import { dtoPostNotNull, PostDataTransferObjectType } from "@/dto/post/dto";
 
 // Action
 import { createdPostAction } from "@/actions/post/created-post-action";
+import { toast } from "react-toastify";
 
 type FormProps = {
   post?: PostDataTransferObjectType;
 };
 
 export function Form({ post }: FormProps) {
-  const [contentValue, setContentValue] = useState(post?.content || "");
-
+  // TODO: Converte comments from portuguese for english.
+  // Assim o state do form sempre é salvo com o último valor enviado e mantido dessa forma.
+  // Aqui preciso chamar o dto novamente pois estes dados não são do componente e sim da server action que será executado juntamente com o hook 'useActionState' para salvar os dados em state da action, então, é necessário realizar o dto para os tipos do objeto não colidam.
   const initialState = {
-    number: 0,
+    valuesFormState: dtoPostNotNull(post),
+    errors: [],
   };
+
   const [state, action, isPending] = useActionState(
     createdPostAction,
     initialState
   );
 
+  // Error
+  useEffect(() => {
+    if (state.errors.length > 0) {
+      toast.dismiss();
+      state.errors.forEach((error) => toast.error(error));
+    }
+  }, [state.errors]);
+
+  // Update
+  useEffect(() => {
+    if (state.success) {
+      toast.dismiss();
+      toast.success("Post atualizado com sucesso!");
+    }
+  }, [state.success]);
+
+  const { valuesFormState } = state;
+  const [contentValue, setContentValue] = useState(post?.content || "");
+
   return (
-    <form action="" className="mt-8">
+    <form action={action} className="mt-8">
       <div className="flex flex-col gap-6">
+        {/* Input Text Area / ID */}
+        <InputText
+          labelText="Id do Post"
+          id="id"
+          name="id"
+          type="text"
+          defaultValue={valuesFormState.id}
+          placeholder={valuesFormState.id}
+          disabled={true}
+          readOnly
+        />
+
         {/* Input Text Area / SLUG */}
         <InputText
           labelText="Slug do Post"
-          name=""
+          id="slug"
+          name="slug"
           type="text"
-          defaultValue={post?.slug || ""}
+          placeholder={valuesFormState.slug}
+          defaultValue={valuesFormState.slug}
+          disabled={true}
           readOnly
         />
 
         {/* Input Text Area / AUTHOR */}
         <InputText
           labelText="Autor do Post"
-          name=""
+          id="author"
+          name="author"
           type="text"
           placeholder="Enter a author post."
-          defaultValue={post?.author || ""}
+          defaultValue={valuesFormState.author}
+          disabled={isPending}
         />
 
         {/* Input Text Area / TITLE */}
         <InputText
           labelText="Título do Post"
-          name=""
+          id="title"
+          name="title"
           type="text"
           placeholder="Enter a title post."
-          defaultValue={post?.title || ""}
+          defaultValue={valuesFormState.title}
+          disabled={isPending}
+        />
+
+        {/* Input Text Area / TITLE */}
+        <InputText
+          labelText="Excerpt do Post"
+          id="excerpt"
+          name="excerpt"
+          type="text"
+          placeholder="Enter a title post."
+          defaultValue={valuesFormState.excerpt}
+          disabled={isPending}
         />
 
         {/* Markdown Editor / MARKDOWN */}
@@ -76,20 +129,24 @@ export function Form({ post }: FormProps) {
         {/* Input Text Area / IMAGE URL */}
         <InputText
           labelText="URL da Imagem de Capa"
+          id="url"
           name="url"
           type="text"
           placeholder="Enter with URL of image"
-          defaultValue={post?.coverImageUrl || ""}
+          defaultValue={valuesFormState.coverImageUrl}
+          disabled={isPending}
         />
 
         {/* Input Checkbox / PUBLISHED */}
         <InputCheckbox
           type="checkbox"
           labelText="Publicar"
-          checked={post?.published}
+          name="published"
+          id="published"
+          disabled={isPending}
         />
 
-        <Button variant="default" size="md" type="submit" className="w-full">
+        <Button disabled={isPending} type="submit" variant="default" size="md">
           Enviar
         </Button>
       </div>
