671f5b6e9fa93ca110d67e6ee786d766ed5622dd
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-21 21:49:33 -0300

    feat: modify from repository method deletePost

--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -1,19 +1,8 @@
 "use server";
 
-// Database
-import { drizzleDatabase } from "@/db/drizzle";
-
-// Queries Cache Admin
-import { findPostByIdCache } from "@/lib/post/queries/admin";
-
-// SQL Queries
-import { eq } from "drizzle-orm";
-
-// Schemas
-import { postsTable } from "@/db/drizzle/schemas";
-
 // Repositories
 import { revalidateTag } from "next/cache";
+import { InstancePostRepository } from "@/repositories/post";
 
 export async function deletePostAction(id: string) {
   // TODO: Check if user logged.
@@ -25,22 +14,23 @@ export async function deletePostAction(id: string) {
     };
   }
 
-  // Get post per Id received.
-  const post = await findPostByIdCache(id).catch(() => undefined);
-
-  if (!post) {
+  let postId;
+  try {
+    postId = await InstancePostRepository.deletePost(id);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return {
+        errors: e.message,
+      };
+    }
     return {
-      error: true,
-      message: "Post not found.",
+      errors: "Erro desconhecido",
     };
   }
 
-  // TODO: Move action that manipulate the removing of posts. /repositories/post/drizzle-post-repository.ts
-  await drizzleDatabase.delete(postsTable).where(eq(postsTable.id, id));
-
   // Revalidate Tags for update data cache in page.
   revalidateTag("posts");
-  revalidateTag(`post-${post.slug}`);
+  revalidateTag(`post-${postId.slug}`);
 
   return {
     error: false,
