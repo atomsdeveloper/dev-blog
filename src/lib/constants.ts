921cda5663385994f5212892b3e90874860d37bd
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-24 18:26:05 -0300

    feat: created constants for set jwt and cookies

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -12,3 +12,21 @@ export const IMAGE_UPLOAD_DIRECTORY_VARIABLE =
 
 export const IMAGE_SERVER_DOMAIN_VARIABLE =
   process.env.IMAGE_UPLOAD_DIRECTORY?.toString() || "";
+
+export const JWT_SECRET_KEY_VARIABLE = process.env.JWT_SECRET_KEY || "";
+
+// Js use mileseconds, so should be multiply per 1000 for get minutes.
+export const LOGIN_EXPIRATION_MINUTES_VARIABLE =
+  Number(process.env.LOGIN_EXPIRATION_SECONDS || 84600) * 1000;
+
+export const LOGIN_EXPIRATION_STRING_VARIABLE =
+  process.env.LOGIN_EXPIRATION_STRING || "1d";
+
+export const LOGIN_COOKIE_NAME_VARIABLE =
+  process.env.LOGIN_COOKIE_NAME || "LOGIN_SESSION";
+
+export const LOGIN_USER_VARIABLE = process.env.LOGIN_USER || "";
+
+export const LOGIN_PASS_VARIABLE = process.env.LOGIN_PASS || "";
+
+export const ALLOW_LOGIN_VARIABLE = Number(process.env.ALLOW_LOGIN || 0);
