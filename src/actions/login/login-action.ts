cdc36ef3ed674ce0060bf91bb0df5c93516d3659
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-24 18:27:26 -0300

    feat: create actions for login

---
+++ src/actions/login/login-action.ts
@@ -0,0 +1,56 @@
+"use server";
+
+// Constants
+import { LOGIN_PASS_VARIABLE, LOGIN_USER_VARIABLE } from "@/lib/constants";
+
+// Login Checks
+import { checkPassword, createLoginSession } from "@/lib/login/manage-login";
+
+// Next
+import { redirect } from "next/navigation";
+
+type LoginActionProps = {
+  username?: string;
+  error: string;
+};
+
+export async function LoginAction(state: LoginActionProps, formData: FormData) {
+  console.log("Login call action.");
+
+  if (!(formData instanceof FormData)) {
+    return {
+      ...state,
+      error: "Dados inválidos",
+    };
+  }
+
+  const username = formData.get("username")?.toString().trim();
+  const pass = formData.get("password")?.toString().trim();
+
+  // Check datas form received exists.
+  if (!username || !pass) {
+    return {
+      ...state,
+      username,
+      error: "Dígite o usuário e a senha",
+    };
+  }
+
+  // Valid datas received with pass hash.
+  const userIsValid = username === LOGIN_USER_VARIABLE || "";
+  const passIsValid = await checkPassword(pass, LOGIN_PASS_VARIABLE || "");
+
+  if (!userIsValid && !passIsValid) {
+    return {
+      ...state,
+      username,
+      error: "Usuário ou senha inválidos",
+    };
+  }
+
+  // Generate cookies for user logged.
+  await createLoginSession(username);
+
+  // Redirect user to pages from admin.
+  redirect("/admin/post");
+}
