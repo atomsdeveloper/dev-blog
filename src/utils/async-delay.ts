3d4a0585701e38cdb1a6276674a440d5229ef432
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-24 19:50:50 -0300

    feat: refactor function

--- src/utils/async-delay.ts
+++ src/utils/async-delay.ts
@@ -1,5 +1,3 @@
-import { SIMULATE_AWAIT_PROMISE_IN_MS_VARIABLE } from "@/lib/constants";
-
 export async function asyncDelay(ms: number, verbose = false) {
   if (ms <= 0) return;
 
@@ -7,7 +5,5 @@ export async function asyncDelay(ms: number, verbose = false) {
     console.log(`Delay of ${ms / 1000}`);
   }
 
-  await new Promise((resolve) =>
-    setTimeout(resolve, SIMULATE_AWAIT_PROMISE_IN_MS_VARIABLE)
-  );
+  await new Promise((resolve) => setTimeout(resolve, ms));
 }
