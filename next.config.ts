43cada781beb5a6a88e39849decb53ff0a2b60c2
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-26 15:56:44 -0300

    fix: add url domain image

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost", "https://dev-blog-eight-drab.vercel.app/"],
+    domains: ["localhost", "https://dev-blog-eight-drab.vercel.app"],
   },
 };
 
