a71a28fac11dc7a6d46a09499971ecb98da7d639
Renan Nascimento <atomsfrontend@gmail.com>
2025-09-10 02:07:05 -0300

    chore: add server action from vercel route

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      // Você pode omitir ou definir se quiser
+      // bodySizeLimit: '1mb', // opcional
+      allowedOrigins: ["https://dev-blog-theta-nine.vercel.app"], // opcional
+    },
+  },
   images: {
     domains: ["localhost", "https://dev-blog-eight-drab.vercel.app"],
   },
